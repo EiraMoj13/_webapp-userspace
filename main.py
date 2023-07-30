349c1aef378bdfd64ad1f97f3555449d8f5fa280
JOMSKII <lumactodjomarie@gmail.com>
2023-07-30 12:30:20 +0800

    add: crud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template, flash,jsonify,json, Response,abort
+from flask import Blueprint, render_template, flash,jsonify, request, json, Response,abort
 from flask_login import login_required, current_user
 from __init__ import create_app, db
 from models import User
@@ -25,15 +25,47 @@ def delete_user(user_id):
     if user:
         db.session.delete(user)
         db.session.commit()
-        return jsonify({'message': 'User deleted successfully'}), 200
+        return jsonify({'status':200,'message': 'User deleted successfully'}), 200
     else:
-        return jsonify({'message': 'User not found'}), 404
+        return jsonify({'status':404,'message': 'User not found'}), 404
     
+@main.route('/user/fetch/<int:user_id>', methods=['GET','POST']) #fetch user
+@login_required
+def fetch(user_id):
+    user = User.query.get(user_id)
+    if user:
+        
+        return jsonify({'status':200, 'id':user_id, 'name':user.name,'email': user.email, 'role':user.role}), 200
+    else:
+        return jsonify({'status':404,'message': 'User not found'}), 404
+
+# Route to handle the update request
+@main.route('/user/update_record/<int:id>', methods=['POST'])
+def update_record(id):
+    record = User.query.get(id)
+
+    if not record:
+        return jsonify({'status':404,'message': 'Record not found'}), 404
+
+    new_name = request.json.get('name')  # Assuming you are updating the 'name' field
+    new_email = request.json.get('email') 
+    new_role= request.json.get('role') 
+    # Update other fields accordingly
+
+    record.name = new_name
+    record.email = new_email
+    record.role = new_role
+    # Update other fields accordingly
+
+    db.session.commit()
+    return jsonify({'status':200, 'message': 'Record updated successfully'}), 200
+   
 @main.route('/manage-users') # manage user page that return 'manage-user'
 @login_required
 def manage_user():
-    if current_user.role == "admin":
-        return render_template('manage-users.html',current_user=current_user)
+    if current_user.role == "admin": 
+        users = User.query.filter(User.id != current_user.id).all()
+        return render_template('manage-users.html',users=users)
     else:
         abort(404, 'Page not found')
 
